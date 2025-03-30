99a041f31dad335fc8d8bc6eaae41b86fa56a20c
Fabrizzio20k <fabrizzio785@gmai.com>
2025-03-30 14:14:52 -0500

    ADD: Redirection to contact pager

--- src/pages/Home/Introduction.tsx
+++ src/pages/Home/Introduction.tsx
@@ -1,14 +1,11 @@
-"use client";
-
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import ThemeToggle from "../../components/AsciiLogo/Picture_Introduction";
 
 export default function Introduction() {
   return (
     <>
-      {/* Contenedor principal */}
       <div className="relative flex flex-col md:flex-row justify-center w-full gap-6 py-10 md:py-28 md:gap-10 md:px-16">
-        {/* Texto a la izquierda */}
         <div className="w-full md:w-1/2 z-10 space-y-6 text-center md:text-left">
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
             Hacemos tus proyectos con inteligencia artificial
@@ -17,12 +14,13 @@ export default function Introduction() {
             Soluciones basadas en IA para empresas
           </p>
           <div className="space-x-4">
-            <Button variant="default" onClick={() => alert("¡Hola!")}>Contactanos</Button>
+            <Link href="/contact" passHref>
+              <Button variant="default">Contáctanos</Button>
+            </Link>
             <Button variant="outline">Prueba Gratuita</Button>
           </div>
         </div>
 
-        {/* Imagen a la derecha */}
         <div className="w-full md:w-1/2 flex justify-center">
           <ThemeToggle />
         </div>
